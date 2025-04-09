485de053ad702f2b4c774c3872e02d25b2663b9b
ChandlerWixom <chandlerj.wixom@gmail.com>
2025-04-09 16:49:57 -0600

    random words add

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
+Random rand = new Random();
+
+
+// past tence verb
+List<string> tenceVerb = new List<string>() { "verbpast1", "verbpast2", "verbpast3", "verbpast4", "verbpast5", "verbpast6", "verbpast7", "verbpast8" };
+
+//plural noun
+List<string> pluralnoun = new List<string>() { "nouns1", "nouns2", "nouns3", "nouns4", "nouns5", "nouns6", "nouns7", "nouns8" };
+
+
+//verb
+List<string> verb = new List<string>() { "verb1", "verb2", "verb3", "verb4", "verb5", "verb6", "verb7", "verb8" };
+
+
+// nouns  pick 10
+List<string> noun = new List<string>() { "noun1", "noun2", "noun3", "noun4", "noun5", "noun6", "noun7", "noun8" };
+
+// adjective
+List<string> adjective = new List<string>() { "BALLS"};
+
+// Makes the dictionary of list
+Dictionary<string, List<string>> wordTypes = new Dictionary<string, List<string>>();
+wordTypes.Add("noun", noun);
+wordTypes.Add("verb", verb);
+wordTypes.Add("past-tense-verb", tenceVerb);
+wordTypes.Add("plural-noun", pluralnoun);
+wordTypes.Add("adjective" , adjective);
+
 
     foreach (string filename in args)
     {
@@ -39,7 +67,7 @@ string[] Words(string filename)
 
             final = final + type[0] + " ";
 
-            final = final + type[1] + " "; 
+            final = final + LookUpReplace(type[1]) + " "; 
             // replace this with the returned vallue later by looking in dictionary and picking one
         }
         else
@@ -51,35 +79,17 @@ string[] Words(string filename)
     Console.WriteLine(final);
   }
 
-// past tence verb
-List<string> tenceVerb = new List<string>() { "verbpast1", "verbpast2", "verbpast3", "verbpast4", "verbpast5", "verbpast6", "verbpast7", "verbpast8" };
-
-//plural noun
-List<string> pluralnoun = new List<string>() { "nouns1", "nouns2", "nouns3", "nouns4", "nouns5", "nouns6", "nouns7", "nouns8" };
-
-
-//verb
-List<string> verb = new List<string>() { "verb1", "verb2", "verb3", "verb4", "verb5", "verb6", "verb7", "verb8" };
-
-
-// nouns  pick 10
-List<string> noun = new List<string>() { "noun1", "noun2", "noun3", "noun4", "noun5", "noun6", "noun7", "noun8" };
-
-
-// Makes the dictionary of list
-Dictionary<string, List<string>> wordTypes = new Dictionary<string, List<string>>();
-wordTypes.Add("noun", noun);
-wordTypes.Add("verb", verb);
-wordTypes.Add("past-tense-verb", tenceVerb);
-wordTypes.Add("plural-noun", pluralnoun);
 
 
 
-//string LookUpReplace(string type)
-//{
-//    wordt
 
-//}
+
+string LookUpReplace(string type)
+{
+   string replace = wordTypes[type][rand.Next(0,wordTypes[type].Count)];
+   return replace;
+
+}
 
 
 
